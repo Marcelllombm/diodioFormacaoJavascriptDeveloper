288c404414a28abab8248fc548389aa43e9462c7
Marcelllombm <melombm@hotmail.com>
2024-06-17 16:05:45 -0300

    variables and operators: diferenças entre a variaveis e tipos de operadores

--- aula_1.js
+++ aula_1.js
@@ -1,9 +1,10 @@
 // let é uma variavel que pode modificar
 let number =  10;
-number = 1;
+
+//tipos de operadores
+number += 10;
+number -= 2;
+number /=2;
+number *=3;
+
 console.log(number)
-// const é uma variavel que não pode modificar
-const variable = 10
-variable = 11;
-// mais mostrar um erro por causa do variable const
-console.log(variable);
