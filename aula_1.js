e0bbc3806f0c2c4faa25af18765d858874d1614b
Marcelllombm <melombm@hotmail.com>
2024-06-17 16:03:38 -0300

    variables and operators: console e como declara variaveis

---
+++ aula_1.js
@@ -0,0 +1,9 @@
+// let é uma variavel que pode modificar
+let number =  10;
+number = 1;
+console.log(number)
+// const é uma variavel que não pode modificar
+const variable = 10
+variable = 11;
+// mais mostrar um erro por causa do variable const
+console.log(variable);
