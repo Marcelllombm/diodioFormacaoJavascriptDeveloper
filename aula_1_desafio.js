d0cd57d7be8f2f9ef49e2742156fff59f844abb3
Marcelllombm <melombm@hotmail.com>
2024-06-18 04:40:28 -0300

    variables and operators: desafio 1

---
+++ aula_1_desafio.js
@@ -0,0 +1,8 @@
+let fuelPrice = 5.79;
+let kilometerPerLiter = 10;
+let distanceTraveled = 100;
+
+let literConsumed  = distanceTraveled / kilometerPerLiter ;
+
+let amountSpent = literConsumed * fuelPrice
+console.log(amountSpent)
