db292a92b30eaa9309c390043490721ca4a4c784
Marcelo Borges <melombm@hotmail.com>
2024-06-20 11:22:50 -0300

    conditional structure: estrutura condicional com else if

--- 02-condicionais/aula_1.js
+++ 02-condicionais/aula_1.js
@@ -1,15 +1,11 @@
-// = é atribuição
-// == compare mesmo se a tipo e diferente
-// === compare se o mesmo tipo e igual
-
-const  number = 10;
-
+const number = 0;
 const evenNumber = (number % 2) === 0;
 console.log(evenNumber)
 
-// operador condicional se
-if(evenNumber){
-    console.log('Par')
-} else {
-    console.log('Impar')
+if(number === 0){
+    console.log('o numero e inválido')
+} else if (evenNumber){
+    console.log('sim, é par')
+}else{
+    console.log('Não e par')
 }
