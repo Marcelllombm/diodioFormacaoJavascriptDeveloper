b2dffa68be772f1984ad87cd4f64ac32cae3a610
Marcelo Borges <melombm@hotmail.com>
2024-06-20 13:52:50 -0300

    conditonal structure: revisão e dicas de boas praticas

--- 02-condicionais/aula_1.js
+++ 02-condicionais/aula_1.js
@@ -1,10 +1,10 @@
 const number = 0;
-const evenNumber = (number % 2) === 0;
+const isEvenNumber = (number % 2) === 0;
 console.log(evenNumber)
 
 if(number === 0){
     console.log('o numero e inválido')
-} else if (evenNumber){
+} else if (isEvenNumber){
     console.log('sim, é par')
 }else{
     console.log('Não e par')
