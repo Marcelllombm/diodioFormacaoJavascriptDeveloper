858e303f4b7eb8d98661c7b21b695b3a7dd57f8e
Marcelllombm <melombm@hotmail.com>
2024-06-24 08:03:21 -0300

    function: conceito básico sobre funções

---
+++ 04-funcoes/01_aula.js
@@ -0,0 +1,13 @@
+/* 
+uma função é um bloco de código projetado para realizar uma tarefa específica que retorna alguma coisa
+funcao que nao retorna nada e procedimento
+*/
+
+function increaseFees(price, percenturalInteret  ){
+    const increasedValue = (percenturalInteret / 100) * price;
+    return price + increasedValue
+}
+
+console.log(increaseFees(1000, 10))
+console.log(increaseFees(700, 10))
+ 
