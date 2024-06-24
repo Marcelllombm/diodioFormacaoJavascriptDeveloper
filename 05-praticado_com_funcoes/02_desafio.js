a3d0659407153682bff2cb073600a62abdde4ce2
Marcelllombm <melombm@hotmail.com>
2024-06-24 09:18:46 -0300

    practicindg: funç

---
+++ 05-praticado_com_funcoes/02_desafio.js
@@ -0,0 +1,11 @@
+// desafio maior de idade
+
+function ofLegalAge(age){
+    if(age > 18){
+        return "vocë e maior de idade"
+    } else {
+    return "vocë e menor de idade"
+    }
+}
+
+console.log(ofLegalAge(17))
