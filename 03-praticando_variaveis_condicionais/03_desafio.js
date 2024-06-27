a9b097c43e612aff420542ef554a9ddc4693540b
Marcelo Borges <melombm@hotmail.com>
2024-06-27 17:30:11 -0300

    objects: estrutura basica do objetos

--- 03-praticando_variaveis_condicionais/03_desafio.js
+++ 03-praticando_variaveis_condicionais/03_desafio.js
@@ -1,31 +1,12 @@
 /*
-    3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento. 
-Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
+3) Elabore em algoritmo que calcule o que deve ser pago por um produto. considerando e preço normal de etiqueta
+e a escolha dea condição de pagamento.
+utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o calculo 
+adequado
 
-Código Condição de pagamento:
-1 - À vista Débito, recebe 10% de desconto;
-2 - À vista no Dinheiro ou PIX, recebe 15% de desconto;
-3 - Em duas vezes, preço normal de etiqueta sem juros;
-4 - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+Código condição de pagamento
+- á vista debito, recebe 10 % de desconto
+- á vista no dinheiro ou pix, recebe 15% de desconto
+- em duas vezes, preço normal de etiqueta sem juros
+- acima de duas vezes, preço normal de etiqueta mais juros de 10%
 */
-
-const productPrice = 100;
-const paymentTerms = 'debito';
-
-if(paymentTerms === 1){
-    const debit = (productPrice * 10) / 100;
-    const amountPaid = productPrice - debit
-    console.log("valor a pagar: R$ "+amountPaid.toFixed(2))
-} else if(paymentTerms === 2) {
-    const debit = (productPrice * 15) / 100;
-    const amountPaid = productPrice - debit;
-    console.log("valor a pagar: R$ "+amountPaid.toFixed(2))
-} else if(paymentTerms === 3){
-    const productPrice = 100;
-    console.log("valor a pagar: R$ "+productPrice.toFixed(2))
-} else {
-    const debit = (productPrice * 10) / 100;
-    const amountPaid = productPrice + debit
-    console.log("valor a pagar: R$ "+amountPaid.toFixed(2))
-}
- 
---
+++ 06-objetos/aula_01.js
@@ -0,0 +1,9 @@
+// objeto é uma coleção de dados , tem chave: e valor
+const marcelo ={
+    nome: 'Marcelo Borges de melo',
+    idade: 40
+}
+
+console.log(marcelo.nome)
+console.log(marcelo.idade)
+console.log(marcelo)
