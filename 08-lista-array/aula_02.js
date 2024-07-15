ebc99a98b3a6d2a4ff09c2aab08fc8e395e31c6c
Marcelo Borges <melombm@hotmail.com>
2024-07-15 13:05:44 -0300

    Arrays: executando for em uma string

--- 08-lista-array/aula_02.js
+++ 08-lista-array/aula_02.js
@@ -3,3 +3,10 @@ for (let index = 0; index < 10; index++) {
     console.log(index)
     
 }
+
+// executando  for em uma string
+const nome = "Marcelo"
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra)
+}
