6f963f72778804fa1bc89e0389eed1656cf7d1ec
Marcelo Borges <melombm@hotmail.com>
2024-07-15 10:01:01 -0300

    Arrays: entendendo a necessidade  de estrutura de repetição

--- 08-lista-array/aula_01.js
+++ 08-lista-array/aula_01.js
@@ -9,3 +9,18 @@ console.log(alunos)
 // remove o ultimo da lista
 alunos.pop();
 console.log(alunos)
+
+// remove o primeiro da lista
+alunos.shift();
+console.log(alunos)
+
+// notas do alunos
+const notas = [];
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+console.log(notas.length)
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4]
+console.log(soma / 5)
