edc29065827384619257d08f64aabcde798a22ae
Marcelo Borges <melombm@hotmail.com>
2024-07-15 14:23:03 -0300

    Arrays: executando for para exibir a m[edia de notas

--- 08-lista-array/aula_02.js
+++ 08-lista-array/aula_02.js
@@ -10,3 +10,18 @@ for (let i = 0; i < nome.length; i++) {
     const letra = nome[i];
     console.log(letra)
 }
+
+// Executando For Para Exibir a Média de Notas
+const notas = [];
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+
+let soma = 0;
+for(let index = 0; index < notas.length; index++) {
+    const nota = notas[index];
+    soma = soma + nota
+}
+console.log(soma / notas.length)
