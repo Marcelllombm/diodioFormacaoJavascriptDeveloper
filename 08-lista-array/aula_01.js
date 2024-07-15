665e120f3fa49bf93fdb01da19b0c10a1b7ddceb
Marcelo Borges <melombm@hotmail.com>
2024-07-15 09:38:34 -0300

    array: adicionando e removendo itens de um array

--- 08-lista-array/aula_01.js
+++ 08-lista-array/aula_01.js
@@ -1,3 +1,11 @@
 //  array - e um lista de dados
 const alunos = ["joão","victor","Marina"];
 console.log(alunos[1])
+
+// adicionar no final da lista
+alunos.push("Marcelo")
+console.log(alunos)
+
+// remove o ultimo da lista
+alunos.pop();
+console.log(alunos)
