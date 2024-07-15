fba44e69b9625f8d28e15c4a16af0d6252997ca5
Marcelllombm <melombm@hotmail.com>
2024-07-15 15:25:01 -0300

    practicing: exercicio tabuada

---
+++ 09-praticando_lista_array/01_desafio.js
@@ -0,0 +1,11 @@
+/*
+    1 crie um programa  que dado um numero imprima a sua tabuada
+
+*/
+
+const number = 5;
+
+for (let index = 0; index <= 10; index++) {
+    console.log(number * index)
+    
+}
