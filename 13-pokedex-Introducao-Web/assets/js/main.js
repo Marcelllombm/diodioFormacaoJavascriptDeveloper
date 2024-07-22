2a130f946d7d78e2b262b8440a84c7f29ce5fc0d
Marcelo Borges <melombm@hotmail.com>
2024-07-22 11:08:22 -0300

    fetch: separando o consumo do api da manipulacao de html

--- 13-pokedex-Introducao-Web/assets/js/main.js
+++ 13-pokedex-Introducao-Web/assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit =10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit},`
-
 function convertPokemonToLi(pokemon){
     return `
     <li class="pokemon">
@@ -16,18 +12,15 @@ function convertPokemonToLi(pokemon){
     </div>
 </li>
     `
-
 }
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
+
+pokeApi.getPokemons().then((pokemons) => {
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i]
         pokemonList.innerHTML += convertPokemonToLi(pokemon)
     }
 })
-.catch((error) => console.error(error))
+
