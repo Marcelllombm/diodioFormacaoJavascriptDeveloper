90d6b928dd5bf300bfb2b07c2beae41c6d099cb0
Marcelllombm <melombm@hotmail.com>
2024-08-19 06:59:42 -0300

    feat: Adicionando o botão de  paginação

--- 13-pokedex-Introducao-Web/assets/js/poke-api.js
+++ 13-pokedex-Introducao-Web/assets/js/poke-api.js
@@ -20,7 +20,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
     .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset=0, limit=20)=>{
+pokeApi.getPokemons = (offset=0, limit=6)=>{
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit},`;
     return fetch(url)
             .then((response) => response.json())
